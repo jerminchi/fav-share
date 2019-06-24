b045b9407cd1354435cee74637ecacf16f96928b
Ch1bi <ch1bi@pop-os.localdomain>
2019-06-24 12:35:59 -0400

    Updates

---
+++ precache-manifest.3171de25c0482948c19d0a690d1e54c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e2f9de68f89e2b71b5deb4b21240537",
+    "url": "/fav-share/static/media/riri2.1e2f9de6.jpg"
+  },
+  {
+    "revision": "3c274aa39f2df9656c47",
+    "url": "/fav-share/static/js/runtime~main.3c274aa3.js"
+  },
+  {
+    "revision": "cc150329c6beced464c4",
+    "url": "/fav-share/static/js/main.cc150329.chunk.js"
+  },
+  {
+    "revision": "438cf1937bc145d23a9b",
+    "url": "/fav-share/static/js/1.438cf193.chunk.js"
+  },
+  {
+    "revision": "cc150329c6beced464c4",
+    "url": "/fav-share/static/css/main.06d2ac2d.chunk.css"
+  },
+  {
+    "revision": "6bdd0d7880873f6181e79e416c16f09d",
+    "url": "/fav-share/index.html"
+  }
+];
